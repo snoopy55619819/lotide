302bbc311fc672df77f48b5fe84948a06d2b0d74
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-11-24 22:22:20 +0000

    Create countOnly() function that goes through a list of items and counts specified items.

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+//Check if values are same and return assertion result.
+const assertEqual = function(actual, expected) {
+  const same = actual === expected;
+  console.log(`${same ? '✅✅✅' : '❌❌❌'}Assertion ${same ? 'Passed' : 'Failed'}: ${actual} ${same ? '===' : '!=='} ${expected}`);
+};
+
+//countOnly will be given an array and an object. 
+// It will return an object containing counts of everything that the input object listed.
+const countOnly = function(allItems, itemsToCount) {
+  const itemCounter = {}; //Create empty counter obj.
+
+  //Iterate though each item in allItems
+  for (const item of allItems) {
+    //Check if item needs to be counted.
+    if (itemsToCount[item] === true){
+      //Add the item to itemCounter obj if not already there. If already in itemCounter then increase by 1.
+      (itemCounter[item] ? itemCounter[item]++ : itemCounter[item] = 1);
+    }
+  }
+  //console.log(itemCounter);
+  return itemCounter;
+};
+
+//Test Cases:
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
