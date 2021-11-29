4d568259ad0693ba17d54a0b3a88d4a0220c3d1b
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-11-29 16:22:34 +0000

    Modify eqArray() function to account for array to be nested, and solve using recursion.

--- eqArrays.js
+++ eqArrays.js
@@ -6,10 +6,25 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let areArraysEqual = true;
+  
   for (let i = 0; i < array1.length; i++) {
-    (array1[i] === array2[i] ? areArraysEqual = true : areArraysEqual = false);
-  }
-  return (array1.length === array2.length ? areArraysEqual : false);
+    const isValue1AnArray = Array.isArray(array1[i]);
+    const isValue2AnArray = Array.isArray(array2[i]);
+
+    if (isValue1AnArray || isValue2AnArray) {
+      if (isValue1AnArray && isValue2AnArray) {
+        const areArraysEqual = eqArrays(array1[i], array2[i]);
+        if(!areArraysEqual) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } else if (!(array1[i] === array2[i])) {
+      return false;
+    }
+  };
+  return (array1.length === array2.length ? true : false);
 };
 
 //Test cases:
@@ -18,8 +33,13 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[[[[[1,2]]]]]], [[[[[[1,2]]]]]]), true); // => true
