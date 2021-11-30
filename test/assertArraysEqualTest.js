157a2d28778467c5be0a69d95d169d97e332392f
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-11-30 00:27:13 +0000

    Update index.js and add uniting testing for some files.

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,14 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
+
+describe ('#assertArraysEqual', () => {
+  it ("returns true for ([1, 2, 3], [1, 2, 3])", () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [1, 2, 3];
+    const expectedOutput = '✅✅✅ Assertion Passed: [1, 2, 3] === [1, 2, 3]';
+    assert.deepEqual(assertArraysEqual(arr1, arr2), undefined);
+  });
+});
 
 // assertArraysEqual([1, 2, 3], [1, 2, 3]);
 // assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'c']);
